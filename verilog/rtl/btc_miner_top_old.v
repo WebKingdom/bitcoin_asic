63a86452ec23515bb6f75ac5a7218f4dda0d451c
Soma Szabo <soma.szabo15@gmail.com>
2022-06-02 21:55:34 -0700

    RTL compileable and mostly done

--- verilog/rtl/btc_miner_top_old.v
+++ verilog/rtl/btc_miner_top_old.v
@@ -20,19 +20,6 @@
  * btc_miner_top
  * TODO!
  *
- * This is an example of a (trivially simple) user project,
- * showing how the user project can connect to the logic
- * analyzer, the wishbone bus, and the I/O pads.
- *
- * This project generates an integer count, which is output
- * on the user area GPIO pads (digital output only).  The
- * wishbone connection allows the project to be controlled
- * (start and stop) from the management SoC program.
- *
- * See the testbenches in directory "mprj_counter" for the
- * example programs that drive this user project.  The three
- * testbenches are "io_ports", "la_test1", and "la_test2".
- *
  *-------------------------------------------------------------
  */
 
